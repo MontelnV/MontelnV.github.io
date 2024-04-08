2cb199cf39773f8503f4f5bdb33bd8c8b5c981e5
Ilya Alikin <iljuha.alikin@gmail.com>
2024-04-08 15:23:58 +0500

    added responsive menu

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+const burger = document.getElementById("burger");
+const menu = document.getElementById("menu");
+let menuLinks = document.querySelectorAll(".responsive_link");
+
+const toggleMenu = () => {
+  burger.classList.toggle("active");
+  menu.classList.toggle("active");
+};
+
+burger.addEventListener("click", () => {
+  toggleMenu();
+});
+
+console.log(menuLinks);
+
+menuLinks.forEach(function (elem) {
+  elem.addEventListener("click", function () {
+    toggleMenu();
+  });
+});
